66a95e2be8065735a300359b52f5aaf3807c55b7
Ritul Singh <ritulsingh00@gmail.com>
2021-03-14 00:04:04 +0530

    Solution For Code Chef Question

---
+++ CodeChef/CONSADD.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+#define l1 long long
+using namespace std;
+l1 a[1005][1005], b[1005][1005];
+bool check(l1 a[1005][1005], l1 b[1005][1005], l1 r, l1 c){
+    l1 i, j;
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            if(a[i][j]!=b[i][j])
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    l1 t, x, r, c;
+    cin >> t;
+    while(t--){
+        cin>>r>>c>>x;
+        l1 i, j, k, diff;
+        for(i=0; i<r; i++){
+            for(j=0; j<c; j++)
+            cin>>a[i][j];
+        }
+        for(i=0; i<r; i++){
+            for(j=0; j<c; j++)
+            cin>>b[i][j];
+        }
+        if(r<x && c<x){
+            if(check(a,b,r,c))
+            cout<<"Yes\n";
+            else
+            cout<<"No\n";
+        }
+        else if(r<x){
+            for(i=0;i<r; i++){
+                for(j=0;j<=(c-x);j++){
+                    if(a[i][j]==b[i][j])
+                    continue;
+                    else{
+                        diff=(b[i][j]-a[i][j]);
+                        a[i][j]+=diff;
+                        k=1;
+                        while(k<x){
+                            a[i][j+k]+=diff;
+                            k++;
+                        }
+                    }
+                }
+            }
+            if(check(a,b,r,c))
+            cout<<"Yes\n";
+            else
+            cout<<"No\n";
+        }
+        else if(c<x){
+            for(i=0;i<c; i++){
+                for(j=0;j<=(r-x);j++){
+                    if(a[j][i]==b[j][i])
+                    continue;
+                    else{
+                        diff=(b[j][i]-a[j][i]);
+                        a[j][i]+=diff;
+                        k=1;
+                        while(k<x){
+                            a[j+k][i]+=diff;
+                            k++;
+                        }
+                    }
+                }
+            }
+            if(check(a,b,r,c))
+            cout<<"Yes\n";
+            else
+            cout<<"No\n";
+        }
+        else if(c>=x && r>=x){
+            for(i=0;i<r; i++){
+                for(j=0;j<=(c-x);j++){
+                    if(a[i][j]==b[i][j])
+                    continue;
+                    else{
+                        diff=(b[i][j]-a[i][j]);
+                        a[i][j]+=diff;
+                        k=1;
+                        while(k<x){
+                            a[i][j+k]+=diff;
+                            k++;
+                        }
+                    }
+                }
+            }
+            for(i=0;i<c;i++){
+                for(j=0;j<=(r-x);j++){
+                    if(a[j][i]==b[j][i])
+                    continue;
+                    else{
+                        diff=(b[j][i]-a[j][i]);
+                        a[j][i]+=diff;
+                        k=1;
+                        while(k<x){
+                            a[j+k][i]+=diff;
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        if(check(a,b,r,c))
+           cout<<"Yes\n";
+           else
+           cout<<"No\n";
+    }
+    return 0;
+}
